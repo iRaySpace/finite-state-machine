924c2d782d182b5f638f99244f20e5b4925acc03
Ivan Ray Altomera <iRaySpacii@gmail.com>
2014-05-07 16:24:40 +0800

    fixing the = instead of ==

--- fsm.lua
+++ fsm.lua
@@ -4,4 +4,4 @@ FSM = class('FSM')
 
 function FSM:initialize(state) self.activeState = state or nil end
 function FSM:setState(state) self.activeState = state end
-function FSM:update() if not self.activeState = nil then self.activeState() end end
+function FSM:update() if not self.activeState == nil then self.activeState() end end
